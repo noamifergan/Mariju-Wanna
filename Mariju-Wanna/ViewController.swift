663b1a1ca155a293cb6dcfc9ca03a664f08ba429
Noam Efergan <noamifergan@gmail.com>
2020-05-30 19:44:02 +0300

    Initial Commit

---
+++ Mariju-Wanna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mariju-Wanna
+//
+//  Created by Noam Efergan on 30/05/2020.
+//  Copyright © 2020 Noam Efergan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
